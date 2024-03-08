e645df1669606cff09373045750e683d0ec415c2
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-08 12:04:02 +0100

    add second options to display loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,18 @@
-const Loader = () => {
+import { BiLoader } from 'react-icons/bi';
+
+type LoaderProps = {
+  type?: string;
+};
+
+const Loader = ({ type }: LoaderProps) => {
+  if (type === 'spinner') {
+    return (
+      <div className={`flex-center w-full h-full absolute`}>
+        <BiLoader className={`animate-spin ${type}`} />
+      </div>
+    );
+  }
+
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
       <div className="loader"></div>
