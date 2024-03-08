ba5bbaad5a5fc800de89e1fbb438084669a2a01c
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-08 12:05:11 +0100

    display data form api, split code to small components and add logic for pagination

--- src/components/cart/CartsList.tsx
+++ src/components/cart/CartsList.tsx
@@ -1,5 +1,63 @@
+import { useState } from 'react';
+import { useCarts } from '../../api/carts/queries/useCarts';
+import { Cart } from '../../types/Cart.type';
+import CartListItem from './CartListItem';
+import CartListTable from './CartListTable';
+import CartListTableHeader from './CartListTableHeader';
+import CartListPagination from './CartListPagination';
+import Error from '../Error';
+import Loader from '../Loader';
+
 const CartsList = () => {
-  return <div>Lista koszykow</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const { isLoading, isError, carts, limit, total } = useCarts(currentPage);
+
+  const totalPages = Math.ceil(total / limit);
+  const nextPage = currentPage < totalPages ? currentPage + 1 : null;
+  const prevPage = currentPage > 1 ? currentPage - 1 : null;
+
+  const handleNextPage = () => {
+    if (nextPage) {
+      setCurrentPage(nextPage);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (prevPage) {
+      setCurrentPage(prevPage);
+    }
+  };
+
+  const paginationProps = {
+    onNextPage: handleNextPage,
+    onPrevPage: handlePrevPage,
+    hasNextPage: !!nextPage,
+    hasPrevPage: !!prevPage,
+    totalPages: totalPages,
+    currentPage: currentPage,
+  };
+
+  if (isLoading) {
+    <Loader type="spinner" />;
+  }
+  if (isError) {
+    <Error />;
+  }
+
+  return (
+    <div className="flex-center flex-col">
+      <h1>All Carts</h1>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-full py-3">
+        <CartListTable>
+          <CartListTableHeader />
+          {carts?.map((cart: Cart) => (
+            <CartListItem key={cart.id} cart={cart} />
+          ))}
+        </CartListTable>
+        <CartListPagination paginationProps={paginationProps} />
+      </div>
+    </div>
+  );
 };
 
 export default CartsList;
