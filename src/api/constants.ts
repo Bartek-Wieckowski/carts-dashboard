5c6c638026adbe96e460e883841df4b5278a2918
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-03-10 21:46:48 +0100

    add new const variable

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,4 +1,6 @@
 export const API_BASE_URL = 'https://dummyjson.com/carts';
+export const API_BASE_URL_PRODUCTS = 'https://dummyjson.com/products';
 export const QUERY_KEY = {
   carts: 'carts',
+  products: 'products',
 };
